bb74559f76da2c6a34acbce6778d7c4379e67403
alokadasanayake976-create <alokadasanayake976@gmail.com>
2026-07-04 10:48:54 +0530

    calling the objects

--- app.js
+++ app.js
@@ -1,22 +1,66 @@
-class Customer{
-    name;
-    age;
-    address;
+// class Customer{
+//     name;
+//     age;
+//     address;
 
-    constructor(name,age,address){
-        this.name =  name;
-        this.age =  age;
-        this.address = address;
+//     constructor(name,age,address){
+//         this.name =  name;
+//         this.age =  age;
+//         this.address = address;
+//     }
+
+// }
+// const Customer1= new Customer("Aloka",20,"Horana");
+//  console.log(Customer1);
+
+let customer2 = {
+    name: "John",
+    age: 25,
+    address: "New York",
+    salary: 50000,
+    items: [
+        {
+            id: 1,
+            name: "item1",
+            price: 100,
+            variants: [
+                {
+                    id: 1,
+                    name: "variant1",
+                    price: 10
+                },
+                {
+                    id: 2,
+                    name: "variant2",
+                    price: 20
+                    
+                }
+            ]
+        }, {
+            id: 2,
+            name: "item2",
+            price: 200
+        }, {
+            id: 3,
+            name: "item3",
+            price: 300
+        }
+    ],
+    father: {
+        name: "Doe",
+        age: 50,
+        address: "New York"
     }
+};
+console.log(customer2.name);
+console.log(customer2.age);
+console.log(customer2.address);
+console.log(customer2.salary);
+console.log(customer2.items[2].name);
+console.log(customer2.father.name);
+console.log(customer2.items[0].variants[1].variants[2]);
 
-}
-const Customer1= new Customer("Aloka",20,"Horana");
- console.log(Customer1);
+ 
 
- let Customer2 = {
-    name = "Devindu",
-    age = 25,
-    address = "new York"
- }
- console.log(Customer2);
+
  
