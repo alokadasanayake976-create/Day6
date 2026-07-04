d40c385b2f415ad95da9c6fd85b394b91d85293e
alokadasanayake976-create <alokadasanayake976@gmail.com>
2026-07-04 11:03:42 +0530

    dom manupulation

--- app.js
+++ app.js
@@ -13,54 +13,59 @@
 // const Customer1= new Customer("Aloka",20,"Horana");
 //  console.log(Customer1);
 
-let customer2 = {
-    name: "John",
-    age: 25,
-    address: "New York",
-    salary: 50000,
-    items: [
-        {
-            id: 1,
-            name: "item1",
-            price: 100,
-            variants: [
-                {
-                    id: 1,
-                    name: "variant1",
-                    price: 10
-                },
-                {
-                    id: 2,
-                    name: "variant2",
-                    price: 20
+// let customer2 = {
+//     name: "John",
+//     age: 25,
+//     address: "New York",
+//     salary: 50000,
+//     items: [
+//         {
+//             id: 1,
+//             name: "item1",
+//             price: 100,
+//             variants: [
+//                 {
+//                     id: 1,
+//                     name: "variant1",
+//                     price: 10
+//                 },
+//                 {
+//                     id: 2,
+//                     name: "variant2",
+//                     price: 20
                     
-                }
-            ]
-        }, {
-            id: 2,
-            name: "item2",
-            price: 200
-        }, {
-            id: 3,
-            name: "item3",
-            price: 300
-        }
-    ],
-    father: {
-        name: "Doe",
-        age: 50,
-        address: "New York"
-    }
-};
-console.log(customer2.name);
-console.log(customer2.age);
-console.log(customer2.address);
-console.log(customer2.salary);
-console.log(customer2.items[2].name);
-console.log(customer2.father.name);
-console.log(customer2.items[0].variants[1].variants[2]);
+//                 }
+//             ]
+//         }, {
+//             id: 2,
+//             name: "item2",
+//             price: 200
+//         }, {
+//             id: 3,
+//             name: "item3",
+//             price: 300
+//         }
+//     ],
+//     father: {
+//         name: "Doe",
+//         age: 50,
+//         address: "New York"
+//     }
+// };
+// console.log(customer2.name);
+// console.log(customer2.age);
+// console.log(customer2.address);
+// console.log(customer2.salary);
+// console.log(customer2.items[2].name);
+// console.log(customer2.father.name);
+// console.log(customer2.items[0].variants[1].variants[2]);
+//---------------------------------------------
+//dom manupulation
+//console.log(document);
+console.log(document.title);
 
- 
+
+
 
 
  
