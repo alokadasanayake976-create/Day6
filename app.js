36d4cd458d198615b0ff2be3d6fbe4cdd4aa93f5
alokadasanayake976-create <alokadasanayake976@gmail.com>
2026-07-10 22:52:44 +0530

    define the months in  a year

--- app.js
+++ app.js
@@ -242,4 +242,9 @@ function updateDateTime() {
         "Sunday","Monday","Tuesday","Wednesday",
         "Thursday","Friday","Saturday"
     ];
+    const months = [
+        "January","February","March","April",
+        "May","June","July","August",
+        "September","October","November","December"
+    ];
 }
